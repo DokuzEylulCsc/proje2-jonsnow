8bc1aef15fc008099225e55f0b476df0fead7f0e
CagatayDeu <cagatay.kapuagasi@ogr.edu.deu.tr>
2019-05-28 19:29:52 +0300

    complate users operations

--- Proje2/UserLists.cs
+++ Proje2/UserLists.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +12,34 @@ namespace Proje2
     class UserLists //üyelerin tutulduğu liste
     {
         List<User> list = new List<User>();
-        User currentUser = null;
+        CurrentUser currentUser = null;
+
+
+        public CurrentUser getCurrent()
+        {
+            return currentUser;
+        }
+
+        public void firstUser(string username)
+        {
+            currentUser = new CurrentUser(list[0].Username, 0);
+        }
+
+        public void updateUserList(string name, string surname, string phone, int index)
+        {
+            User temp = list[index];
+            temp.Name = name;
+            temp.Surname = surname;
+            temp.PhoneNumber = phone;
+
+            list[index] = temp;
+        }
 
         public bool loadCurrentUser(string username)
         {
+            int index = 0;
+
+
             if (list.Count == 0)
                 return false;
 
@@ -20,34 +47,78 @@ namespace Proje2
             {
                 if (i.Username == username)
                 {
-                    currentUser = i;
+                    currentUser = new CurrentUser(i.Username, index);
+                    currentUser.Name = i.Name;
+                    currentUser.Surname = i.Surname;
+                    currentUser.PhoneNumber = i.PhoneNumber;
+
+                    foreach (Reservation a in i.ReservationList)
+                    {
+                        if ((a.EndDate - DateTime.Now).TotalHours > 0)
+                            currentUser.ReservationList.Add(a);
+                        else
+                            currentUser.HistoryReserve.Add(a);
+                    }
+
                     return true;
                 }
-                else
-                    return false;
+
+                index++;
             }
 
             return false;
         }
 
-        public void addUsers(string username)
+        public bool addUsers(string username)
         {
-            list.Add(new User(username));
+            bool possible = true;
+
+            if (list.Count == 0)
+            {
+                list.Add(new User(username));
+                return true;
+            }
+
+            foreach (User i in list)
+            {
+                if (i.Username == username)
+                {
+                    possible = false;
+                }
+            }
+
+            if (possible)
+                list.Add(new User(username));
+
+
+            return possible;
         }
+
         public void loadTxt()
         {
+            string stream_read = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "datas.txt");
+
+            if (stream_read.Length > 10)
+                list = JsonConvert.DeserializeObject<List<User>>(stream_read);
+
+
+            foreach (User i in list)
+                Debug.WriteLine(i.PhoneNumber);
 
         }
 
         public void saveTxt()
         {
-
+            var datas = JsonConvert.SerializeObject(list);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "datas.txt", datas);
         }
+
         public void loginAdmin()
         {
-
+            list.Clear();
+            var datas = JsonConvert.SerializeObject(list);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "admin.txt", datas);
         }
 
     }
-
 }
