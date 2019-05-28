e7ac6524b82f28a4a3ac6f592dccf3134c273c15
semihdeu <48005227+semihdeu@users.noreply.github.com>
2019-05-28 22:14:50 +0300

    Account Management.cs düzenlendi

--- Proje2/AccountManagement.cs
+++ Proje2/AccountManagement.cs
@@ -6,48 +6,75 @@ using System.Threading.Tasks;
 
 namespace Proje2
 {
-    class AccountManagement //giriş yönetimi
+    public class AccountManagement //giriş yönetimi
     {
         UserLists list = new UserLists();
+        internal CurrentUser currentUser = null;
 
-        public AccountManagement(string loginType)
+        public AccountManagement(bool isAdmin)
         {
-            if (loginType == "user")
-                loginUser();
-            else
-                loginAdmin();
+            loginAdmin();
         }
 
-
-        void loginUser()
+        public AccountManagement()
         {
 
+        }
+
+        public void firstUser(string username)
+            //İlk kullanıcı
+        {
+            list.firstUser(username);
+            currentUser = list.getCurrent();
+        }
+
+        public void updateUserList(string name, string surname, string phone, int index)
+            //Anlık kullanıcıyı kaybetmemek için 
+        {
+            list.updateUserList(name, surname, phone, index);
+        }
+
+        public bool loginUser(string username)
+        {
+            bool temp = list.loadCurrentUser(username);
+            if (temp != false)
+            {
+                currentUser = list.getCurrent();
+            }
+
+            return temp;
+        }
+
+        public bool Register(string username)
+            //Girilen username'i userlist e kayıt ediyoruz.
+        {
+            bool temp = list.addUsers(username);
+            if (temp != false)
+            {
+                list.loadCurrentUser(username);
+                currentUser = list.getCurrent();
+            }
+
+            return temp;
+        }
+
+        public void loadTxt()
+            //Önceden kayıtlı kullanıcıları tutuyoruz..
+        {
+            list.loadTxt();
+        }
+
+        public void saveTxt()
+            //Kullanıcıyı database kayıt ediyoruz.
+        {
+            
+            list.saveTxt();
         }
 
         void loginAdmin()
+            //Admin girişi
         {
-
+            list.loginAdmin();
         }
-        public void firstUser()
-        {
-
-        }
-        public void updateUserList()
-        {
-
-        }
-        public bool Register()
-        {
-            return false;
-        }
-        public void loadTxt()
-        {
-        
-        }
-        public void saveTxt()
-        {
-         
-        }
-
     }
 }
