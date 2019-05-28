2bd7ac97f514e038855f2f547e295090d3b5a763
FurkanColmabey <33626342+FurkanColmabey@users.noreply.github.com>
2019-05-28 19:35:13 +0300

    bitti

--- Proje2/CurrentUser.cs
+++ Proje2/CurrentUser.cs
@@ -8,18 +8,44 @@ namespace Proje2
 {
     class CurrentUser : User //geçerli kullanıcı
     {
-        public CurrentUser(string username) : base(username)
-        {
+        int index;
 
-        }
-        public bool makeReservation()
+        public CurrentUser(string username, int index) : base(username)
         {
-            return false;
-        }
-        public void cancelReservation()
-        {
-
+            this.index = index;
         }
 
+        public int Index
+        {
+            get { return index; }
+        }
+
+        public bool makeReservation(DateTime startDate, DateTime endDate, Hotel hotel, Room room)
+        {
+            bool temp = true;
+
+            foreach (Reservation i in ReservationList)
+            {
+                if (i.StartDate < endDate && startDate < i.EndDate && hotel == i.Hotel && room.No == i.Room.No)
+                {
+                    temp = false;
+                    break;
+                }
+            }
+
+            if (temp)
+            {
+                Reservation reservation = new Reservation(startDate, endDate, hotel, room);
+                ReservationList.Add(reservation);
+            }
+
+            return temp;
+
+        }
+
+        public void cancelReservation(Reservation reservation)
+        {
+            ReservationList.Remove(reservation);
+        }
     }
 }
