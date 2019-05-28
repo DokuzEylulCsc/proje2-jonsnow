c8573b6bc8ce5cff7e5ce3d92567cd62d0c3ea07
semihdeu <48005227+semihdeu@users.noreply.github.com>
2019-05-28 22:07:35 +0300

    TripleRoom command satırları düzenlendi.

--- Proje2/TripleRoom.cs
+++ Proje2/TripleRoom.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Proje2
 {
-    class TripleRoom: Room //üçlü oda
+    class TripleRoom : Room //üçlü oda
     {
+        private bool isSpecialPool;
+
+        public TripleRoom(int floor, int no, bool isFull, bool isHaveJacuzzi, bool isSpecialPool, int price) : base(floor, no, isFull, isHaveJacuzzi, price)
+        {
+            this.isSpecialPool = isSpecialPool;
+        }
+        // Üçlü oda class'ı
+        public bool IsSpecialPool
+        {
+            get { return isSpecialPool; }
+        }
     }
 }
