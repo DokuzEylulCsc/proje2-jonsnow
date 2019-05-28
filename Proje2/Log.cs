3c47d97f580776388803eb7e20e28ed08d5f2c7c
CagatayDeu <cagatay.kapuagasi@ogr.edu.deu.tr>
2019-05-28 19:17:50 +0300

    Write exception to txt

--- Proje2/Log.cs
+++ Proje2/Log.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proje2
 {
-    class Log //loglar buradan yazılacak
+    class Log
     {
-        public void addLog()
+        public void addLog(Exception e)
         {
+            string stream_read = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt"); // mevcut logları okur
+            stream_read += Environment.NewLine + e.GetType() + " " + e.StackTrace + Environment.NewLine + "Tarih: " + DateTime.Now; //logların üstüne yeni satıra geçerek
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", stream_read);//tüm logları dosyaya yazar          //hatanın tipini, nerden kaynaklandığını yazar
+        }                                                                                                                          //ve zamanını yazar.
 
-        }
     }
 }
